d7909d77cc8ce3c1c8b463c1ee2742cd5aa563bc
Samim Aktar <samimaktar0773@gmail.com>
2021-10-04 08:30:39 +0530

    Done UC1 Add PLayer

--- src/sample/TicTacToe.java
+++ src/sample/TicTacToe.java
@@ -3,8 +3,19 @@ package sample;
 public class TicTacToe {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Tic Tac Toe Game: ");
+		PlayerStartFreshTicTacToe();
 		
 	}
+	
+	public static void PlayerStartFreshTicTacToe() {
+			char creatBoard [] = new char [10];
+			for ( int i = 1; i<creatBoard.length; i++ ) {
+				 creatBoard [i];
+				
+			}
+			
+			
+		}
 	
 	
 	
